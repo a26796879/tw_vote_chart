98d4dbe46c7ac228cfb3f59bc034d99b2d525c6a
a26796879 <33983387+a26796879@users.noreply.github.com>
2022-02-04 15:54:06 +0800

    add flask for backend

---
+++ flask_vote_data.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('learn.html')
+
+@app.route('/Tw_Town.js')
+def Tw_Town():
+    return render_template('Tw_Town.js')
+
+@app.route('/learn_df.js')
+def learn_df():
+    return render_template('learn_df.js')
+
+@app.route('/taoyuan/')
+def taoyuan():
+    return render_template('learn.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
